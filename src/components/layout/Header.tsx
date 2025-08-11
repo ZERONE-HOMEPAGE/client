92ae100c86f9d2b64d9f73fed7a7a86edb189ea1
PkHamin <jqkrgkals123@gmail.com>
2025-08-12 04:04:53 +0900

    feat: 상단 네비게이션 구성(#15)
    - 스터디페이지 연결
    - 스터디페이지에 헤더 컴포넌트 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,24 @@
+import { Link, NavLink } from 'react-router-dom';
+
 export default function Header() {
-    return (
-        <header className="position-sticky z-50 bg-white p-4">
-            <h1>헤더섹션</h1>
-        </header>
-    );
+  return (
+    <header className="position-sticky z-50 bg-white p-4 flex flex-row justify-evenly">
+      <h1 className="">
+        <Link to="/">zerone</Link>
+      </h1>
+
+      <div>
+        <ul className="list-none flex space-x-5 ">
+          <li>
+            <NavLink to="/study" className={({ isActive }) => (isActive ? 'font-bold' : '')}>
+              스터디
+            </NavLink>
+          </li>
+          <li>활동</li>
+          <li>게시판</li>
+          <li>Q&A</li>
+        </ul>
+      </div>
+    </header>
+  );
 }
