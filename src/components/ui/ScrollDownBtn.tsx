2b91beffa560a1b30a10cd03e515a46f74be876f
mokmokahr <mokmokahr@users.noreply.github.com>
2025-08-27 20:56:06 +0900

    feat: 모바일 히어로섹션 제작

--- src/components/ui/ScrollDownBtn.tsx
+++ src/components/ui/ScrollDownBtn.tsx
@@ -3,8 +3,8 @@ import useScrollTo from '@/hooks/useScrollTo';
 export default function ScrollDownBtn() {
     const { scrollDown } = useScrollTo();
     return (
-        <div className="flex flex-col items-center">
-            <div onClick={scrollDown} className="p-3 hover:cursor-pointer animate-bounce absolute bottom-10 left-1/2 transform -translate-x-1/2 flex flex-col items-center gap-2">
+        <div className="absolute bottom-10 left-0 right-0 flex justify-center">
+            <div onClick={scrollDown} className="p-3 hover:cursor-pointer animate-bounce flex flex-col items-center gap-2">
                 <p className="text-lg text-white">Scroll</p>
                 <div className="w-4 h-4 border-r-2 border-b-2 border-gray-100 rotate-45"></div>
             </div>
