953d6cd3815841bb9849effdea1ed2746308e0bc
hanbi <207645224+hanbi06@users.noreply.github.com>
2025-08-28 22:26:29 +0900

    feat: PillTab 애니메이션 효과 추가(#45)
    애니메이션 효과 추가했습니다.
    이동시간은 0.3초로 고정입니다.

--- src/components/ui/PillTab.tsx
+++ src/components/ui/PillTab.tsx
@@ -1,11 +1,14 @@
 export default function PillTab({tabElements, clickHandler, activeTabIdx, textclass}: { tabElements: { label: string, active: boolean }[], clickHandler: (index: number) => void, activeTabIdx: number, textclass?: string }) {
     return (
-        <div className="flex justify-evenly p-1 bg-[#E0D7F1] rounded-3xl gap-2">
+        <div className="relative flex justify-evenly p-1 bg-[#E0D7F1] rounded-3xl gap-2">
             {tabElements.map((element, index) => (
-                <div onClick={() => clickHandler(index)} key={index} className={`p-2 px-4 rounded-3xl hover:cursor-pointer ${activeTabIdx === index ? 'bg-white' : ''}`}>
+                <button key={index} onClick={() => clickHandler(index)} className="relative z-10 py-2 w-32 text-center">
                     <p className={`${textclass}`}>{element.label}</p>
-                </div>
+                </button>
             ))}
+            <div className="absolute p-2 px-4 bg-white w-32 rounded-3xl h-[calc(100%-0.5rem)] transition-all ease-in-out duration-300"
+                 style={{ left: `calc(${activeTabIdx} * (100% / ${tabElements.length}) + 0.3rem)` }}>
+            </div>
         </div>
     );
 }
